72fa0cc11b99dad84804b1282fe30fbda476e707
Derek Razo <derekrazo@Dereks-MacBook-Pro.local>
2014-10-02 22:29:00 -0700

    fixed buttons, navbar linking, and mobile spacing

--- client/js/cobudget-landing-2.js
+++ client/js/cobudget-landing-2.js
@@ -1,30 +1,13 @@
 if (Meteor.isClient) {
   
-  // jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
-      if ($(".navbar").offset().top > 50) {
-          $(".navbar-fixed-top").addClass("top-nav-collapse");
-      } else {
-          $(".navbar-fixed-top").removeClass("top-nav-collapse");
-      }
-  });
-
-  // jQuery for page scrolling feature - requires jQuery Easing plugin
-  $(function() {
-      $('a.page-scroll').bind('click', function(event) {
-          var $anchor = $(this);
-          $('html, body, .page').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1500, 'easeInOutExpo');
-          event.preventDefault();
-      });
-  });
-
-  // Closes the Responsive Menu on Menu Item Click
-  $('.navbar-collapse ul li a').click(function() {
-      $('.navbar-toggle:visible').click();
-  });
-
+  $('a[href^="#"]').on('click',function (e) {
+	     e.preventDefault();
+        $(this).fadeOut(1000);
+        $("body, html").animate({ 
+            scrollTop: $( $(this).attr('href') ).offset().top 
+        }, 900);
+	});
+  
 
 
 
